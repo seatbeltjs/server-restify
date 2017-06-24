b73e7520325f1a2c9fe904aa32522eaa3d672e82
Thomas Meadows <thomasmeadows@gmail.com>
2017-06-24 15:23:04 -0400

    cleaned up code, added missing typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,15 @@
 import * as restify from 'restify';
+import { Server } from '@seatbelt/core/lib/server';
 import { Log } from '@seatbelt/core';
-export declare class RestifyServer {
+export interface IServerConfig {
+    port?: number;
+}
+export declare class RestifyServer implements Server.BaseServer {
     log: Log;
     server: restify.Server;
     port: number;
+    constructor(config?: IServerConfig);
     conformServerControllerToSeatbeltController: Function;
-    config: Function;
-    init: Function;
+    config: Server.Config;
+    init: Server.Init;
 }
-export declare const server: RestifyServer;
--- lib/index.ts
+++ lib/index.ts
@@ -1,18 +1,35 @@
 import * as restify from 'restify';
-import { DServerRegister, IServerRequest, IServerResponse, IServerRoute, Log } from '@seatbelt/core';
+import { Server } from '@seatbelt/core/lib/server';
+import { Log } from '@seatbelt/core';
 
-@DServerRegister()
-export class RestifyServer {
+export interface IServerConfig {
+  port?: number;
+}
+
+@Server.Register()
+export class RestifyServer implements Server.BaseServer {
   public log: Log = new Log('RestifyServer');
   public server: restify.Server = restify.createServer();
   public port: number = process.env.port || 3000;
-  public conformServerControllerToSeatbeltController: Function = function (route: IServerRoute, req: restify.Request, res: restify.Response) {
 
-    const seatbeltResponse: IServerResponse = {
-      send: (status: number, body: any) => res.send(status, body)
+  public constructor(config?: IServerConfig) {
+    if (config) {
+      if (config.port && typeof config.port === 'number') {
+        this.port = config.port;
+      }
+    }
+  }
+
+  public conformServerControllerToSeatbeltController: Function = function (route: Server.Route, req: restify.Request, res: restify.Response) {
+
+    const seatbeltResponse: Server.Response = {
+      send: (status: number, body: Object) => {
+        res.status(status);
+        return res.send(body);
+      }
     };
 
-    const seatbeltRequest: IServerRequest = {
+    const seatbeltRequest: Server.Request = {
       allParams: Object.assign(
         {},
         typeof req.query === 'object' ? req.query : {},
@@ -31,24 +48,24 @@ export class RestifyServer {
     );
 
   };
-  public config: Function = function(routes: IServerRoute[]) {
+
+  public config: Server.Config = function(routes: Server.Route[]) {
     this.server.use(restify.bodyParser());
     this.server.use(restify.queryParser());
 
     if (routes && Array.isArray(routes)) {
-      routes.forEach((route: IServerRoute) => {
-        route['__seatbelt_config__'].type.forEach((eachType: string) => {
-          route['__seatbelt_config__'].path.forEach((eachPath: string) => {
+      routes.forEach((route: Server.Route) => {
+        route['__seatbeltConfig'].type.forEach((eachType: string) => {
+          route['__seatbeltConfig'].path.forEach((eachPath: string) => {
             this.server[eachType.toLowerCase()](eachPath, (req: restify.Request, res: restify.Response) => this.conformServerControllerToSeatbeltController(route, req, res));
           });
         });
       });
     }
   };
-  public init: Function = function () {
+
+  public init: Server.Init = function () {
     this.log.system(`starting server on ${this.port}`);
     this.server.listen(this.port);
   };
 }
-
-export const server: RestifyServer = new RestifyServer();
